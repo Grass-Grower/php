daad619e34f2d10de0cf80762125b09e812d27a8
Grass-Grower <164552274+Grass-Grower@users.noreply.github.com>
2024-08-24 20:27:59 +0000

    copied code

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Example PHP page</title>
-  </head>
-  <body>
-    <?php
-      echo '<h1>Hello world!</h1>';
-      echo '<p>This page uses PHP version ' . phpversion() . '.</p>';
-    ?>
-  </body>
-</html>
+<php
+
+$mysqli = mysqli_connect("ep-soft-sunset-a478dq9k.us-east-1.pg.koyeb.app", "koyeb-adm", "yQ4Z6bOzjvgP", "koyebdb");
+
+if (mysqli_connect_errno()) {
+  printf("Connect failed: $s\n", mysqli_connect_errno());
+  exit();
+} else {
+  $sql = ""; //SQL Commands
+  $res = mysqli_query($mysqli, $sql);
+  if ($res === TRUE) {
+    echo mysqli_get_host_info($mysqli);
+  } else {
+    printf("Could not perform action: $s\n", mysqli_error($mysqli));
+  }
+  mysqli_close($mysqli);
+}
