29e6dbf24369fe5ae685e908cbd8c0b04581e619
Grass-Grower <164552274+Grass-Grower@users.noreply.github.com>
2024-10-03 17:58:14 +0000

    Made index.php

--- index.php
+++ index.php
@@ -1,30 +1,9 @@
-<?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-// sql to create table
-$sql = "CREATE TABLE Users (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)";
-
-if ($conn->query($sql) === TRUE) {
-  echo "Table Users created successfully";
-} else {
-  echo "Error creating table: " . $conn->error;
-}
-
-$conn->close();
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My PHP</title>
+  </head>
+  <body>
+    <p>search: <?php echo $_GET['search'] ?></p>
+  </body>
+</html>
